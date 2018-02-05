60d73f3fdb70f5323172b2ed97ca01ff09deb265
jaquawn <jd23@hawaii.edu>
2018-02-05 10:52:55 -1000

    just added too icons on the navigation

--- Hackwitch3/ViewController.swift
+++ Hackwitch3/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.firstLabel.text = "about"
     }
 
     override func didReceiveMemoryWarning() {
